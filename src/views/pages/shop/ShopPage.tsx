900cd9e7e66a9fe96c4b1b8f59f918f94779dadf
Mahbub Hasan <mahbubhasan.pro@gmail.com>
2023-11-23 15:17:11 +0600

    Pagination

--- src/views/pages/shop/ShopPage.tsx
+++ src/views/pages/shop/ShopPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ProductCard from "../../../components/ProductCard/ProductCard";
 
 function ShopPage() {
@@ -91,12 +92,24 @@ function ShopPage() {
       productPrice: 199,
     },
   ];
+  const itemsPerPage = 10; // Number of items to display per page
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // Calculate the indexes for the current page
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = products.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
       <div>
         <div>
           <div className="py-4 text-center text-primary dark:text-secondary">
-            <h2 className="text-lg sm:text-xl md:text-2xl tracking-wide after:block after:border-b-2 after:w-20 after: after:mx-auto after:my-1 after:dark:border-primary">
+            <h2 className="text-lg sm:text-xl md:text-2xl tracking-wide after:block after:border-b-2 after:w-20 after: after:mx-auto after:my-1 after:dark:border-darkPrimary">
               Shop
             </h2>
           </div>
@@ -139,18 +152,41 @@ function ShopPage() {
             </div>
 
             <div className="col-span-6 md:col-span-5 px-6">
-              <div className="bg-slate-50 flex justify-between p-3 text-xs rounded-md">
+              <div className="bg-slate-50 flex justify-between p-3 text-xs rounded-md text-primary shadow-sm dark:bg-darkPrimary dark:text-secondary">
                 <div>
-                  <p>There are 5 products</p>
+                  <p>{`There are ${products.length} products`}</p>
                 </div>
                 <div>
                   <p>Sort by price, high to low</p>
                 </div>
               </div>
               <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 py-4">
-                {products?.map((product, index) => {
+                {currentItems?.map((product, index) => {
                   return <ProductCard key={index} productInfo={product} />;
                 })}
+              </div>
+
+              <div className="bg-slate-50 flex justify-between p-3 text-xs rounded-md text-primary shadow-md dark:bg-darkPrimary dark:text-secondary">
+                <div>
+                  <p>{`Showing ${indexOfFirstItem + 1}-${
+                    products.length < indexOfLastItem
+                      ? products.length
+                      : indexOfLastItem
+                  } of ${products.length} item(s)`}</p>
+                </div>
+                <div>
+                  {Array.from({
+                    length: Math.ceil(products.length / itemsPerPage),
+                  }).map((_, index) => (
+                    <button
+                      key={index}
+                      onClick={() => paginate(index + 1)}
+                      className="p-2"
+                    >
+                      {index + 1}
+                    </button>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
